6450bddfa1f94bc16133b1d31a2059b82849b680
Motheo Motlhakane <motheomotlhakane10@gmail.com>
2026-06-30 23:36:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_Level_Interfaces_with_Files_Tutorial_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
